479ccdfdfb08f16f3b815b446afc00827a604554
Patrick <dev@engeplabs.com>
2024-06-17 00:48:28 +0200

    Only have single on change handler and handle test suites and test cases. Fix #32 by using recursive function.

--- public/js/test_plan_page.js
+++ public/js/test_plan_page.js
@@ -24,59 +24,56 @@ function deselectAllTestPlanCases() {
     $('#test_plan_data').val('');
 }
 
-
 /****************************************************************************
  CHECKBOXES
  ****************************************************************************/
 
 $(document).ready(function () {
-    // checkbox test suite
-    $("body").on("change", ".test_suite_cbx", function () {
-
-        var test_suite_id = $(this).attr('data-test_suite_id');
-
-        if (this.checked) {
-            $(`.test_case_cbx[data-test_suite_id=${test_suite_id}]`).prop('checked', true); // mark all the suite's test cases
-            $(`.test_suite_cbx[data-parent_id=${test_suite_id}]`).click(); //
-        } else {
-            $(`.test_case_cbx[data-test_suite_id=${test_suite_id}]`).prop('checked', false);
-            $(`.test_suite_cbx[data-parent_id=${test_suite_id}]`).click();
+    $("body").on("change", ".test_suite_cbx, .test_case_cbx", function () {
+        if ($(this).hasClass("test_suite_cbx")) {
+            // go up to the tree_suite element and update all children to the new value
+            $(this).closest(".tree_suite").find(".test_suite_cbx, .test_case_cbx").prop("checked", $(this).prop("checked"));
         }
+        // update the parent suite(s) state
+        updateParentSuites($(this));
 
         updateFormDataField();
     });
 
-    $("body").on("change", ".test_case_cbx", function () {
-
-        var test_suite_id = $(this).attr('data-test_suite_id');
-
-        var test_suite_selector = `.test_suite_cbx[data-test_suite_id=${test_suite_id}]`;
-        var test_case_selector = `.test_case_cbx[data-test_suite_id=${test_suite_id}]`;
-
-        // find all test cases with the specified test suite id
-        var status = [];
-        $(test_case_selector).each(function (index) {
-            status.push($(this).prop('checked'))
-        });
-
-        if (status.includes(false)) {
-            $(test_suite_selector).prop('checked', false);
-        } else {
-            $(test_suite_selector).prop('checked', true);
+    /**
+     * Recursive function that updates all parent suites of a test suite or test case.
+     *
+     * @param $element - jQuery Element of test suite or test case checkbox
+     */
+    function updateParentSuites($element) {
+        // check, if we are on a test suite checkbox and get parent
+        const parentTestSuiteId = $element.hasClass("test_suite_cbx") ?
+            $element.attr("data-parent_id") :
+            $element.attr("data-test_suite_id");
+        if (parentTestSuiteId) {
+            const $parentTestSuite = $(`.test_suite_cbx[data-test_suite_id=${parentTestSuiteId}]`)
+            const childTestSuiteSelector = `.test_suite_cbx[data-parent_id=${parentTestSuiteId}]`;
+            const childTestCaseSelector = `.test_case_cbx[data-test_suite_id=${parentTestSuiteId}]`;
+            // check if all children are selected
+            const allChildrenChecked = $parentTestSuite
+                .closest(".tree_suite")
+                .find([childTestSuiteSelector, childTestCaseSelector].join())
+                .toArray()
+                .every(function (childElement) {
+                    // normal DOM element, not jQuery element
+                    return childElement.checked;
+                });
+            $parentTestSuite.prop("checked", allChildrenChecked);
+            // continue with parent
+            updateParentSuites($parentTestSuite);
         }
-
-        updateFormDataField();
-    });
-
+    }
 
     function updateFormDataField() {
-        var selected = [];
-
+        const selected = [];
         $('input.test_case_cbx:checked').each(function () {
             selected.push($(this).attr('data-test_case_id'));
         });
-
         $('#test_plan_data').val(selected);
     }
-
 });

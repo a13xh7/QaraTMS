dcf09679840af8cfd8cce3bd1b9aac81aa949f88
Patrick Engelhardt <dev@engeplabs.com>
2024-06-17 08:32:47 +0200

    Fix #32 Test Suite Selection (#35)
    * Make selecting all test cases not rely on click handler for the test suite checkboxes
    
    * Reduce redundant code by selecting all checkboxes in single jQuery selector
    
    * Translate russian comments to english
    
    * Only have single on change handler and handle test suites and test cases. Fix #32 by using recursive function.
    
    * eslint

--- public/js/test_plan_page.js
+++ public/js/test_plan_page.js
@@ -5,79 +5,78 @@ function renderPlanTree(select) {
 }
 
 function selectAllTestPlanCases() {
-    $(".test_suite_cbx").each(function (index) {
-        $(this).prop('checked', false);
-        $(this).click();
+    const selectedTestCases = [];
+    $(".test_suite_cbx, .test_case_cbx").each(function (index) {
+        $(this).prop('checked', true)
+        const testCaseId = $(this).attr('data-test_case_id');
+        // add test case ids to selectedTestCases
+        if (testCaseId !== undefined) {
+            selectedTestCases.push(testCaseId);
+        }
     });
+    $('#test_plan_data').val(selectedTestCases);
 }
 
 function deselectAllTestPlanCases() {
-    $(".test_suite_cbx").each(function (index) {
+    $(".test_suite_cbx, .test_case_cbx").each(function () {
         $(this).prop('checked', false);
     });
-
-    $(".test_case_cbx").each(function (index) {
-        $(this).prop('checked', false);
-    });
-
     $('#test_plan_data').val('');
 }
-
 
 /****************************************************************************
  CHECKBOXES
  ****************************************************************************/
 
 $(document).ready(function () {
-
-
-    // Чекбокс test suite
-    $("body").on("change", ".test_suite_cbx", function () {
-
-        var test_suite_id = $(this).attr('data-test_suite_id');
-
-        if (this.checked) {
-            $(`.test_case_cbx[data-test_suite_id=${test_suite_id}]`).prop('checked', true);  // отметить все текст кейсы сьюта
-            $(`.test_suite_cbx[data-parent_id=${test_suite_id}]`).click(); //
-        } else {
-            $(`.test_case_cbx[data-test_suite_id=${test_suite_id}]`).prop('checked', false);
-            $(`.test_suite_cbx[data-parent_id=${test_suite_id}]`).click();
+    $("body").on("change", ".test_suite_cbx, .test_case_cbx", function () {
+        if ($(this).hasClass("test_suite_cbx")) {
+            // go up to the tree_suite element and update all children to the new value
+            $(this)
+                .closest(".tree_suite")
+                .find(".test_suite_cbx, .test_case_cbx")
+                .prop("checked", $(this).prop("checked"));
         }
+        // update the parent suite(s) state
+        updateParentSuites($(this));
 
         updateFormDataField();
     });
 
-    $("body").on("change", ".test_case_cbx", function () {
-
-        var test_suite_id = $(this).attr('data-test_suite_id');
-
-        var test_suite_selector = `.test_suite_cbx[data-test_suite_id=${test_suite_id}]`;
-        var test_case_selector = `.test_case_cbx[data-test_suite_id=${test_suite_id}]`;
-
-        // найти все тест кейсы  с указанным test suite id
-        var status = [];
-        $(test_case_selector).each(function (index) {
-            status.push($(this).prop('checked'))
-        });
-
-        if (status.includes(false)) {
-            $(test_suite_selector).prop('checked', false);
-        } else {
-            $(test_suite_selector).prop('checked', true);
+    /**
+     * Recursive function that updates all parent suites of a test suite or test case.
+     *
+     * @param $element - jQuery Element of test suite or test case checkbox
+     */
+    function updateParentSuites($element) {
+        // check, if we are on a test suite checkbox and get parent
+        const parentTestSuiteId = $element.hasClass("test_suite_cbx") ?
+            $element.attr("data-parent_id") :
+            $element.attr("data-test_suite_id");
+        if (parentTestSuiteId) {
+            const $parentTestSuite = $(`.test_suite_cbx[data-test_suite_id=${parentTestSuiteId}]`)
+            const childTestSuiteSelector = `.test_suite_cbx[data-parent_id=${parentTestSuiteId}]`;
+            const childTestCaseSelector = `.test_case_cbx[data-test_suite_id=${parentTestSuiteId}]`;
+            // check if all children are selected
+            const allChildrenChecked = $parentTestSuite
+                .closest(".tree_suite")
+                .find([childTestSuiteSelector, childTestCaseSelector].join())
+                .toArray()
+                .every(function (childElement) {
+                    // normal DOM element, not jQuery element
+                    return childElement.checked;
+                });
+            $parentTestSuite.prop("checked", allChildrenChecked);
+            // continue with parent
+            updateParentSuites($parentTestSuite);
         }
-
-        updateFormDataField();
-    });
-
+    }
 
     function updateFormDataField() {
-        var selected = [];
-
+        const selected = [];
         $('input.test_case_cbx:checked').each(function () {
             selected.push($(this).attr('data-test_case_id'));
         });
-
         $('#test_plan_data').val(selected);
     }
-
 });

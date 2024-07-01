3034f7e3f7b7f649a21380d1b42d55deb3452d8f
Patrick <patrick@tie-international.com>
2024-07-01 21:21:22 +0200

    group css together and remove emptylines

--- resources/views/layout/base_layout.blade.php
+++ resources/views/layout/base_layout.blade.php
@@ -4,37 +4,25 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>QaraTMS - Open Source Test Management Tool</title>
-
     <link rel="icon" type="image/x-icon" href="{{asset('/img/favicon.ico')}}">
-
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.min.css">
-
+    <link href="{{asset('css/main.css')}}" rel="stylesheet">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://code.jquery.com/ui/1.13.1/jquery-ui.min.js"></script>
     <script src="{{asset('js/js.cookie.min.js')}}"></script>
-
-    <link href="{{asset('css/main.css')}}" rel="stylesheet">
-
     @yield('head')
 </head>
 <body>
-
 <div class="row sticky-top">
     @include('layout.header_nav')
 </div>
-
 <div class="container-fluid">
-
     <div class="row fh">
-
         @yield('content')
-
     </div>
-
 </div>
-
 <div class="modal fade" id="any_img_lightbox" tabindex="-1" aria-labelledby="exampleModalCenterTitle"
      style="display: none;" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered">
@@ -43,7 +31,6 @@
         </div>
     </div>
 </div>
-
 <script src="{{asset('js/main.js')}}"></script>
 @yield('footer')
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"

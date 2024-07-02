21d8ba8978a96c399e6778f7abe1ab22271454bf
Patrick Engelhardt <dev@engeplabs.com>
2024-07-02 11:16:13 +0200

    Refactor base layout and navs (#44)
    * group css together and remove emptylines
    
    * Remove line breaks from nav templates
    
    * update bootstrap
    
    * Create redirect from index to /projects to be able to set the start page from index in the future
    
    * Fix typo
    
    * Remove more whitespaces and add nbsp between icon and text of nav items
    
    * Make links in header nav translateable
    
    * Adapt tests to new logic for index route and update UserFactory as well as properly link it to the User model
    
    ---------
    
    Co-authored-by: Patrick <patrick@tie-international.com>

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,28 +1,27 @@
 <?php
 
-/** @var \Illuminate\Database\Eloquent\Factory $factory */
+namespace Database\Factories;
 
 use App\User;
-use Faker\Generator as Faker;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Str;
 
-/*
-|--------------------------------------------------------------------------
-| Model Factories
-|--------------------------------------------------------------------------
-|
-| This directory should contain each of the model factory definitions for
-| your application. Factories provide a convenient way to generate new
-| model instances for testing / seeding your application's database.
-|
-*/
+class UserFactory extends Factory
+{
 
-$factory->define(User::class, function (Faker $faker) {
-    return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-        'remember_token' => Str::random(10),
-    ];
-});
+    protected $model = User::class;
+
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'email_verified_at' => Carbon::now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ];
+    }
+}
--- resources/views/layout/base_layout.blade.php
+++ resources/views/layout/base_layout.blade.php
@@ -4,37 +4,25 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>QaraTMS - Open Source Test Management Tool</title>
-
     <link rel="icon" type="image/x-icon" href="{{asset('/img/favicon.ico')}}">
-
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
-          integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.min.css">
-
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link href="{{asset('css/main.css')}}" rel="stylesheet">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://code.jquery.com/ui/1.13.1/jquery-ui.min.js"></script>
     <script src="{{asset('js/js.cookie.min.js')}}"></script>
-
-    <link href="{{asset('css/main.css')}}" rel="stylesheet">
-
     @yield('head')
 </head>
 <body>
-
 <div class="row sticky-top">
     @include('layout.header_nav')
 </div>
-
 <div class="container-fluid">
-
     <div class="row fh">
-
         @yield('content')
-
     </div>
-
 </div>
-
 <div class="modal fade" id="any_img_lightbox" tabindex="-1" aria-labelledby="exampleModalCenterTitle"
      style="display: none;" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered">
@@ -43,11 +31,10 @@
         </div>
     </div>
 </div>
-
 <script src="{{asset('js/main.js')}}"></script>
 @yield('footer')
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm"
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
         crossorigin="anonymous"></script>
 </body>
 </html>

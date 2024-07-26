dc9c8fef599c22e5abd5c5402510f953a17dae7a
alexhqa <a.harcenco@copybet.com>
2024-07-26 11:24:52 +0300

    title fix

--- resources/views/layout/base_layout.blade.php
+++ resources/views/layout/base_layout.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>QaraTMS - Open Source Test Management Tool</title>
+    <title>QaraTMS - Open Source Test Management System</title>
     <link rel="icon" type="image/x-icon" href="{{asset('/img/favicon.ico')}}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
